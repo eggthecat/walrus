2cb951c0a176cd5854731e7e4ff22772e6a8413a
Dominic Montelongo and Heather Zurek <dominic@>
2019-03-26 11:15:07 -0700

    Add buttons styling for js

--- js/scripts.js
+++ js/scripts.js
@@ -8,3 +8,25 @@ $(document).ready(function() {
     $("#initially-hidden1").toggle();
   });
 });
+
+$(function() {
+  $("button#green").click(function() {
+    $("body").removeClass();
+    $("body").addClass("green-background");
+  });
+
+  $("button#yellow").click(function() {
+    $("body").removeClass();
+    $("body").addClass("yellow-background");
+  });
+
+  $("button#red").click(function() {
+    $("body").removeClass();
+    $("body").addClass("red-background");
+  });
+
+  $("button#blue").click(function() {
+    $("body").removeClass();
+    $("body").addClass("blue-background");
+  });
+});
